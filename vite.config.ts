8fb985ebf7d5192e087729e708b6935ba3c19a17
wenzhe <mysteryven@gmail.com>
2022-08-30 21:32:16 +0800

    chore: update build tool

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -7,8 +8,25 @@ export default defineConfig({
   esbuild: {
     jsxInject: `import React from 'react'`
   },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.tsx'),
+      name: 'useShadow',
+      fileName: 'index'
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+      
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom'
-},
+  },
 })
